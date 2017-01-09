4a35c5cedd8494f7a5f3538bb53b9de3a8797267
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2017-01-10 01:45:45 +0900

    refactoring

--- change_settings.rb
+++ change_settings.rb
@@ -1,18 +1,16 @@
-# change build settings
+# frozen_string_literal: true
 require 'xcodeproj'
 
 project_path = './NKJMovieComposer.xcodeproj'
 project = Xcodeproj::Project.open(project_path)
 
 project.targets.each do |target|
-
-  puts "#{target.name}"
+  puts "Target is #{target.name}"
   target.build_configurations.each do |config|
     key = 'SWIFT_VERSION'
     config.build_settings[key] ||= '3.0.1'
     puts "changed #{key}(#{config.build_settings[key]})"
   end
-
 end
 
 project.save
