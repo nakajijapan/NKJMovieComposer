3e88c4c9799c6613b62a768570378ff80ce2e73a
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2017-01-10 01:40:35 +0900

    empty line

--- change_settings.rb
+++ change_settings.rb
@@ -1,4 +1,6 @@
+# change build settings
 require 'xcodeproj'
+
 project_path = './NKJMovieComposer.xcodeproj'
 project = Xcodeproj::Project.open(project_path)
 
@@ -6,11 +8,11 @@ project.targets.each do |target|
 
   puts "#{target.name}"
   target.build_configurations.each do |config|
-
     key = 'SWIFT_VERSION'
     config.build_settings[key] ||= '3.0.1'
     puts "changed #{key}(#{config.build_settings[key]})"
-
   end
+
 end
+
 project.save
