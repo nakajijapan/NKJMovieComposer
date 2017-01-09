596cde691f3f1e01d51c108f84c36d81283d27f5
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2017-01-10 03:42:42 +0900

    use xcworkspace

--- change_settings.rb
+++ change_settings.rb
@@ -2,24 +2,31 @@
 require 'xcodeproj'
 
 # settings
-project_paths = [
-  './NKJMovieComposer.xcodeproj',
-  './NKJMovieComposerDemo.xcodeproj'
-]
+path =  './NKJMovieComposer.xcworkspace'
 
 def change_build_settings(project)
+  puts "Project path(#{project.path})"
+  project.build_configurations.each do |config|
+    change_swift_version(config)
+  end
+
   project.targets.each do |target|
     puts "Target is #{target.name}"
     target.build_configurations.each do |config|
-      key = 'SWIFT_VERSION'
-      config.build_settings[key] = '3.0.1'
-      puts "changed #{key}(#{config.build_settings[key]})"
+      change_swift_version(config)
     end
   end
 end
 
-project_paths.each do |path|
-  project = Xcodeproj::Project.open(path)
+def change_swift_version(config)
+  key = 'SWIFT_VERSION'
+  config.build_settings[key] = '3.0.1'
+  puts "changed #{key}(#{config.build_settings[key]})"
+end
+
+workspace = Xcodeproj::Workspace.new_from_xcworkspace(path)
+workspace.schemes.each do |key, value|
+  project = Xcodeproj::Project.open(value)
   change_build_settings(project)
   project.save
 end
